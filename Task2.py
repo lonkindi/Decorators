b96aa722c902b9f27661241b22e8af8079b59dc7
lonkindi <legions.ru@gmail.com>
2020-04-02 17:45:34 +0500

    Final commit

--- Task2.py
+++ Task2.py
@@ -1,31 +1,31 @@
 import datetime
 
 
-def logger(some_func):
-    log_str = ''
+def set_log_path(log_path):
+    def logger(some_func):
+        log_str = ''
 
-    def new_some_func(*args, **kwargs):
-        log_path = 'some_log2.txt'
-        if kwargs.get('log'):
-            log_path = kwargs['log']
-        with open(log_path, 'a', encoding='utf-8') as logfile:
-            nonlocal log_str
-            log_str = f'{datetime.datetime.now()} : '
-            log_str += f'функция <{some_func.__name__}>'
-            log_str += f', аргументы *args: <{args}>, **kwargs: <{kwargs}>'
-            result = some_func(*args, **kwargs)
-            log_str += f'- возвращаемое значение <{result}>'
-            logfile.write(f'{log_str}\n')
-            return result
+        def new_some_func(*args, **kwargs):
+            with open(log_path, 'a', encoding='utf-8') as logfile:
+                nonlocal log_str
+                log_str = f'{datetime.datetime.now()} : '
+                log_str += f'функция <{some_func.__name__}>'
+                log_str += f', аргументы *args: <{args}>, **kwargs: <{kwargs}>'
+                result = some_func(*args, **kwargs)
+                log_str += f'- возвращаемое значение <{result}>'
+                logfile.write(f'{log_str}\n')
+                return result
 
-    return new_some_func
+        return new_some_func
+
+    return logger
 
 
-@logger
+@set_log_path('log2.txt')
 def square(*args, **kwargs):
     a = 10
     return a ** 2
 
 
 if __name__ == '__main__':
-    print(square('some_arg', log='log2.txt'))
+    print(square('some_arg', kwarg='some_kwarg'))
