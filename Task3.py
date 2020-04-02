a5ef56669ca1938fcc5cf3041ed19e74fe04beb2
lonkindi <legions.ru@gmail.com>
2020-04-02 17:40:44 +0500

    Commit

--- Task3.py
+++ Task3.py
@@ -1,27 +1,27 @@
 import datetime
 
 
-def logger(some_func):
-    log_str = ''
+def set_log_path(log_path):
+    def logger(some_func):
+        log_str = ''
 
-    def new_some_func(*args, **kwargs):
-        log_path = 'some_log3.txt'
-        if kwargs.get('log'):
-            log_path = kwargs['log']
-        with open(log_path, 'a', encoding='utf-8') as logfile:
-            nonlocal log_str
-            log_str = f'{datetime.datetime.now()} : '
-            log_str += f'функция <{some_func.__name__}>'
-            log_str += f', аргументы *args: <{args}>, **kwargs: <{kwargs}>'
-            result = some_func(*args, **kwargs)
-            log_str += f'- возвращаемое значение <{result}>'
-            logfile.write(f'{log_str}\n')
-            return result
+        def new_some_func(*args, **kwargs):
+            with open(log_path, 'a', encoding='utf-8') as logfile:
+                nonlocal log_str
+                log_str = f'{datetime.datetime.now()} : '
+                log_str += f'функция <{some_func.__name__}>'
+                log_str += f', аргументы *args: <{args}>, **kwargs: <{kwargs}>'
+                result = some_func(*args, **kwargs)
+                log_str += f'- возвращаемое значение <{result}>'
+                logfile.write(f'{log_str}\n')
+                return result
 
-    return new_some_func
+        return new_some_func
+
+    return logger
 
 
-@logger
+@set_log_path('log3.txt')
 def adv_print(*args, **kwargs):
     """
     adv_print(text: str, start_text: str, max_line=int, in_file=path_to_file: str)
@@ -51,4 +51,4 @@ def adv_print(*args, **kwargs):
 
 if __name__ == '__main__':
     adv_print('Контакт с именем Jhon и фамилией Smit в телефонной книге не найден', 'start_text', max_line=10,
-              in_file='adv_print.txt', log='log3.txt')
+              in_file='adv_print.txt')
